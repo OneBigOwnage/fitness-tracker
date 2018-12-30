ab471e5cf28056274ed7fd037ba17f7d5838a322
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-30 20:55:23 +0100

    Split header and sidenav into their own components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { TrainingComponent } from './training/training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MAT_DATE_LOCALE } from '@angular/material';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { MAT_DATE_LOCALE } from '@angular/material';
     NewTrainingComponent,
     PastTrainingComponent,
     TrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
